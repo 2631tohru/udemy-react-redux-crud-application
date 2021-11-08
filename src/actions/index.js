5c8ea3729a6930e98a0416d5a26e3610ad014515
2631tohru <26312631tohru@gmail.com>
2021-11-08 17:46:17 +0900

    inplemnet event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 export const READ_EVENTS = "READ_EVENTS"
+export const CREATE_EVENT = "CREATE_EVENT"
 
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1"
 const QUERYSTRYING = '?token=token123'
@@ -8,4 +9,10 @@ export const readEvents = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRYING}`)
   console.log(response);
   dispatch({ type: READ_EVENTS, response })
+
+}
+export const postEvent = values => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRYING}`, values)
+  console.log(response);
+  dispatch({ type: CREATE_EVENT, response })
 }
