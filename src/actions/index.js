b29ec8d56dd078f1b7bc9ab4af33db3f176d6777
2631tohru <26312631tohru@gmail.com>
2021-11-08 20:31:09 +0900

    implement eventsShow

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 export const READ_EVENTS = "READ_EVENTS"
+export const READ_EVENT = "READ_EVENT"
 export const CREATE_EVENT = "CREATE_EVENT"
+export const UPDATE_EVENT = "UPDATE_EVENT"
+export const DELETE_EVENT = "DELETE_EVENT"
 
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1"
 const QUERYSTRYING = '?token=token123'
@@ -13,6 +16,20 @@ export const readEvents = () => async dispatch => {
 }
 export const postEvent = values => async dispatch => {
   const response = await axios.post(`${ROOT_URL}/events${QUERYSTRYING}`, values)
-  console.log(response);
   dispatch({ type: CREATE_EVENT, response })
+}
+
+export const deleteEvent = id => async dispatch => {
+  await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRYING}`)
+  dispatch({ type: DELETE_EVENT, id })
+}
+
+export const getEvent = id => async dispatch => {
+ const response = await axios.get(`${ROOT_URL}/events/${id}${QUERYSTRYING}`)
+ dispatch({ type: READ_EVENT, response })
+}
+
+export const putEvent = values => async dispatch => {
+ const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERYSTRYING}`, values)
+ dispatch({ type: UPDATE_EVENT, response })
 }
--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,13 +3,20 @@ import { connect } from "react-redux"
 import { Field, reduxForm } from 'redux-form'  
 import { Link } from "react-router-dom"; 
 
-import { postEvent } from "../actions"
+import { getEvent, deleteEvent, putEvent } from "../actions"
 
-class EventsNew extends Component {
+class EventsShow extends Component {
   constructor(props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
+    this.onDeleteClick = this.onDeleteClick.bind(this)
+    
   } 
+
+  componentDidMount() {
+      const { id } = this.props.match.params
+      if (id) this.props.getEvent(id)
+  }
   renderField(field) {
     const { input, label, type, meta: { touched, error }} = field
 
@@ -20,9 +27,15 @@ class EventsNew extends Component {
       </div>
     )
   }
+  
+  async onDeleteClick() {
+    const { id } = this.props.match.params
+    await this.props.deleteEvent(id)
+    this.props.history.push('/')
+  }
 
   async onSubmit(values) {
-    await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
   
@@ -36,13 +49,19 @@ class EventsNew extends Component {
         <div>
           <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
       </form>
    )
   }
 }
 
-const mapDispatchToProps = ({ postEvent })
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
+const mapStateToProps = (state, ownProps) => {
+    const event = state.events[ownProps.match.params.id]
+    return {initialValues: event, event}
+}
+
 const validate = values => {
   const errors = {}
 
@@ -52,7 +71,7 @@ const validate = values => {
   return errors
 }
 
-export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: "eventNewForm"})(EventsNew)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  reduxForm({ validate, form: "eventShowForm", enableReinitialize: true})(EventsShow)
 )
 
